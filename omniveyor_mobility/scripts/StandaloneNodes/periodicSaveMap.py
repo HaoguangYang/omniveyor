967fd78e54853f6ac8f15aaa9d171877dc31baf0
Haoguang Yang <yang1510@purdue.edu>
2022-06-27 12:25:04 -0400

    debug map saving utility

--- omniveyor_mobility/scripts/StandaloneNodes/periodicSaveMap.py
+++ omniveyor_mobility/scripts/StandaloneNodes/periodicSaveMap.py
@@ -3,13 +3,14 @@
 from slam_toolbox_msgs.srv import SaveMap, SerializePoseGraph
 from std_msgs.msg import String
 from spatio_temporal_voxel_layer.srv import SaveGrid
+import time
 import rospy
 import rospkg
 
 if __name__ == "__main__":
     rospy.init_node("map_saving_utility")
-    mapPath = rospy.get_param("map_save_path", rospkg.RosPack().get_path('omniveyor_mobility')+'/resources/maps/map')
-    interval = rospy.get_param("map_save_interval", 60.0)
+    mapPath = rospy.get_param("~map_save_path", rospkg.RosPack().get_path('omniveyor_mobility')+'/resources/maps/map')
+    interval = rospy.get_param("~map_save_interval", 60.0)
     if (interval < 0.):
         raise RuntimeError("ERROR: map saving interval must not be a negative number!")
     planarMapSaveSrvUp = False
@@ -17,9 +18,17 @@ if __name__ == "__main__":
     save2dMap = None
     save3dMap = None
 
-    f = rospy.Rate(1./interval)
+    f = rospy.Rate(1.0)
+    startupTime = time.time() + interval
 
     while(not rospy.is_shutdown()):
+
+        f.sleep()
+
+        if (time.time() <= startupTime):
+            continue
+        startupTime = time.time() + interval
+        
         if (not planarMapSaveSrvUp):
             try:
                 rospy.wait_for_service("slam_toolbox/serialize_map", timeout=1.0)
@@ -39,27 +48,23 @@ if __name__ == "__main__":
             except rospy.ROSException as ex:
                 print ("WARNING: Service 'spatiotemporal_voxel_grid/save_grid' is down")
 
-        f.sleep()
-
         if (planarMapSaveSrvUp):
             try:
                 resp = save2dMap[0](mapPath)
-                print("PoseGraph saved with response: " + resp)
+                print("PoseGraph saved with response: " + str(resp))
             except rospy.ServiceException as exc:
                 print("WARNING: Service 'slam_toolbox/serialize_map' did not process request: " + str(exc))
             try:
                 resp = save2dMap[1](String(mapPath))
-                print("pgm saved with response: " + resp)
+                print("pgm saved with response: " + str(resp))
             except rospy.ServiceException as exc:
                 print("WARNING: Service 'slam_toolbox/save_map' did not process request: " + str(exc))
         
         if (pointCloudSaveSrvUp):
             try:
                 resp = save3dMap[0](String(mapPath+"_1.vdb"))
-                print("vdb (front) saved with response: " + resp)
+                print("vdb (front) saved with response: " + str(resp))
                 resp = save3dMap[1](String(mapPath+"_2.vdb"))
-                print("vdb (rear) saved with response: " + resp)
+                print("vdb (rear) saved with response: " + str(resp))
             except rospy.ServiceException as exc:
                 print("WARNING: Service 'spatiotemporal_voxel_grid/save_grid' did not process request: " + str(exc))
-
-    
